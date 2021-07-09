f3a94286a00a89bb9971b92076f19237a5702a39
JeongYunMi <tamd5971@gmail.com>
2021-07-10 07:14:08 +0900

    main페이지 기본 디자인

--- src/components/home/content.jsx
+++ src/components/home/content.jsx
@@ -18,7 +18,7 @@ const ShowTodo = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: flex-start;
   align-content: flex-start;
 `;
 
@@ -27,7 +27,47 @@ const content = () => {
     <ContentWrap>
       <Menu />
       <ShowTodo>
-        <TodoCard color="#967A6D" />
+        <TodoCard
+          color="#967A6D"
+          todoContent="투두 리스트 작성"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#A7C4DA"
+          todoContent="학교갔다가 잠자기"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#E5BBB4"
+          todoContent="수업 자료 만들기"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#967A6D"
+          todoContent="10시전에 일어나기"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#E4D8D3"
+          todoContent="알고리즘 과제하기"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#E5BBB4"
+          todoContent="그리디 알고리즘 공부하기"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#E4D8D3"
+          todoContent="세계 문화와 기독교 발표 준비하기"
+          date="2021.07.10"
+        />
+        <TodoCard
+          color="#818D90"
+          todoContent="중국어 워크북 P.26~29"
+          date="2021.07.10"
+        />
+        <TodoCard color="#A7C4DA" todoContent="CBT 신청" date="2021.07.10" />
       </ShowTodo>
     </ContentWrap>
   );
--- src/components/home/todoCard.jsx
+++ src/components/home/todoCard.jsx
@@ -1,14 +1,72 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  AiOutlineCheckCircle,
+  AiOutlineEdit,
+  AiOutlineDelete,
+} from "react-icons/ai";
 
 const CardWrap = styled.div`
   width: 200px;
   height: 160px;
-  background-color: ${(props) => console.log("d", props.color)};
+  background-color: ${(props) => props.color};
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-content: space-between;
+  border-radius: 8px;
 `;
 
-const todoCard = () => {
-  return <CardWrap>카드!</CardWrap>;
+const TopWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 32px;
+  background-color: #ffffff7d;
+`;
+
+const IConButton = styled.div`
+  width: 32px;
+  height: 32px;
+  margin-top: 4px;
+  text-align: center;
+`;
+
+const MiddleWarp = styled.div`
+  padding: 4px 8px 0;
+  height: 104px;
+`;
+
+const BottomWarp = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CreateDate = styled.div`
+  padding-top: 8px;
+  padding-left: 8px;
+`;
+
+const todoCard = ({ color, todoContent, date }) => {
+  return (
+    <CardWrap color={color}>
+      <TopWrap>
+        <IConButton>
+          <AiOutlineEdit size="24" />
+        </IConButton>
+        <IConButton>
+          <AiOutlineCheckCircle size="24" />
+        </IConButton>
+      </TopWrap>
+      <MiddleWarp>{todoContent}</MiddleWarp>
+      <BottomWarp>
+        <CreateDate>{date}</CreateDate>
+        <IConButton>
+          <AiOutlineDelete size="24" />
+        </IConButton>
+      </BottomWarp>
+    </CardWrap>
+  );
 };
 
 export default todoCard;
