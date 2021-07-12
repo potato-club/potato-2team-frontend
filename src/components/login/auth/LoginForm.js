a0ed12663d8561ff96232ba66282d7e0595028d5
JeongYunMi <tamd5971@gmail.com>
2021-07-12 22:32:33 +0900

    form 수정

--- src/components/login/auth/LoginForm.js
+++ src/components/login/auth/LoginForm.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-
-
- const PageExplans = styled.div`
+const PageExplans = styled.div`
   display: flex;
   justify-content: left;
   align-items: center;
@@ -12,9 +10,9 @@ import { Link } from 'react-router-dom';
   padding: 0 0 15px 0;
   font-weight: bold;
   color: currentcolor;
- `;
+`;
 
- const Input = styled.input`
+const Input = styled.input`
   max-width: 100%;
   padding: 15px 20px;
   background: #f9f9fa;
@@ -28,25 +26,23 @@ import { Link } from 'react-router-dom';
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
   :focus,
   :hover {
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
- }
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
+  }
 `;
 
-
 const LoginButton = styled.button`
- max-width: 100%;
- padding: 11px 13px;
- color: white;
- font-weight: 600;
- text-transform: uppercase;
- background: #ffd5d9;
- border: none;
- border-radius: 3px;
- cursor: pointer;
- margin-top: 0.6rem;
- box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
- transition: all 0.3s ease-out;
- 
+  max-width: 100%;
+  padding: 11px 13px;
+  color: white;
+  font-weight: 600;
+  text-transform: uppercase;
+  background: #ffd5d9;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  margin-top: 0.6rem;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-out;
 `;
 
 const Sign = styled(Link)`
@@ -61,26 +57,44 @@ const Label = styled.div`
   color: grey;
 `;
 
-
 const LoginForm = () => {
+  const [loginData, setLoginData] = useState({
+    userId: "",
+    userPwd: "",
+  });
+
+  const onChangeForm = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+    console.log("롸:", loginData);
+  };
+
+  const loginSubmit = async () => {
+    try {
+      console.log("//await // 보내는 뭐시기 axios~~~~");
+    } catch (e) {
+      console.log("에러발생!");
+    }
+    //다른페이지 넘겨주자
+  };
+
   return (
     <>
-    <PageExplans>로그인</PageExplans>
-    <Label>이메일</Label>
-    <Input 
-     name= "id"
-     placeholder="이메일"
-    />
-    <Label>비밀번호</Label>
-    <Input
-      name="password"
-      type="password"
-      placeholder="비밀번호"
-    />
-    <LoginButton>로그인</LoginButton>
-    <Sign to="/register">회원가입</Sign>
+      <PageExplans>로그인</PageExplans>
+      <Label>이메일</Label>
+      <Input name="userId" placeholder="이메일" onChange={onChangeForm} />
+      <Label>비밀번호</Label>
+      <Input
+        name="userPwd"
+        type="password"
+        placeholder="비밀번호"
+        onChange={onChangeForm}
+      />
+      <LoginButton onClick={loginSubmit}>로그인</LoginButton>
+      <Sign to="/register">회원가입</Sign>
     </>
-    
   );
 };
 
